f6853e8d0cb0c833cb200f661f47989e839d5356
Krishna K <krishnakarthik1999@gmail.com>
2024-03-01 13:11:52 +0530

    module 1.4 complete

--- Module 1/Module1.4/t1.py
+++ Module 1/Module1.4/t1.py
@@ -6,7 +6,7 @@ import re
 td_count = 0
 continent_count = 1
 
-tokens = ('BEGIN', 'OPENROW', 'CLOSEROW', 'OPENHEADER', 'OPENTABLE',
+tokens = ('BEGIN', 'OPENROW', 'CLOSEROW', 'OPENHEADER', 'OPENTABLE', 'OPENHREF', 'CLOSEHREF',
           'CLOSEHEADER', 'PATTERN', 'CONTENT', 'OPENDATA', 'CLOSEDATA', 'THEADOPEN', 'THEADCLOSE', 'GARBAGE')
 t_ignore = '\t '
 
@@ -25,6 +25,14 @@ def t_THEADOPEN(t):
 
 def t_THEADCLOSE(t):
     r'</thead>'
+    return t
+
+def t_OPENHREF(t):
+    r'<a[^>]*>'
+    return t
+
+def t_CLOSEHREF(t):
+    r'</a[^>]*>'
     return t
 
 def t_OPENDATA(t):
@@ -51,13 +59,14 @@ def t_CLOSEHEADER(t):
     r'</th[^>]*>'
     return t
 
+def t_PATTERN(t):
+    r'(&\#160;)|(&amp;)|(&\#91;[0-9]&\#93;)|\#|(&[a-z]*;)'
+    return t
+
 def t_CONTENT(t):
     r'[A-Za-z0-9,+\(\)\-–\/\.:\' ]+'
     return t
 
-def t_PATTERN(t):
-    r'(&\#160;)|(&amp;)|(&\#91;[0-9]&\#93;)|\#'
-    return t
 
 def t_GARBAGE(t):
     r'(<[^>]*>)|(<\/[^>]*>)|(<[^\/>]*\/>)|<![^>]>'
@@ -104,9 +113,13 @@ def p_onlydata(p):
     '''
     onlydata : OPENDATA CLOSEDATA comment 
              | OPENDATA GARBAGE CONTENT GARBAGE CLOSEDATA comment
+             | OPENDATA OPENHREF CONTENT CLOSEHREF CLOSEDATA comment
+             | OPENDATA OPENHREF CONTENT PATTERN CONTENT CLOSEHREF CLOSEDATA comment
              | OPENDATA GARBAGE GARBAGE CLOSEDATA comment
              | OPENDATA CONTENT CLOSEDATA comment 
     '''
+    if(len(p) == 9):
+        p[3] = 'Réunion'
     global td_count,continent_count
     td_count = td_count+1
     if continent_count != 7 and continent_count <= 8:
@@ -140,10 +153,42 @@ def p_onlydata(p):
                 the_file.write("==============================================\n")
                 continent_count = continent_count+1
                 td_count = 0
-    else:
+    elif continent_count==7:
         if td_count == 22:
             continent_count = continent_count+1
             td_count = 0
+    else:
+        with open('countries.txt', 'a') as the_file:
+            if td_count == 2:
+                if p.slice[3].type != 'CONTENT':
+                    the_file.write(f'Country: {p[2]}\n') #in case of world
+                else:
+                    the_file.write(f'Country: {p[3]}\n')
+            elif td_count == 3 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Total Cases: {p[2]}\n')
+            elif td_count == 4 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'New Cases: {p[2]}\n')
+            elif td_count == 5 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Total Deaths: {p[2]}\n')
+            elif td_count == 6 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'New Deaths: {p[2]}\n')
+            elif td_count == 7 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Total Recovered: {p[2]}\n')
+            elif td_count == 8 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'New Recovered: {p[2]}\n')
+            elif td_count == 9 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Active Cases: {p[2]}\n')
+            elif td_count == 12 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Deaths Million: {p[2]}\n')
+            elif td_count == 13 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Total Tests: {p[2]}\n')
+            elif td_count == 14 and p.slice[2].type == 'CONTENT':
+                the_file.write(f'Tests Million: {p[2]}\n')
+            if td_count == 22:
+                the_file.write("==============================================\n")
+                continent_count = continent_count+1
+                td_count = 0
+
 
 def p_error(p):
     
@@ -154,12 +199,12 @@ def p_error(p):
     pass
 
 def main():
-    # req = Request('https://www.worldometers.info/coronavirus/',headers ={'User-Agent':'Mozilla/5.0'})
-    # webpage = urlopen(req).read()
-    # mydata = webpage.decode("utf8")
-    # f=open('webpage.html','w',encoding="utf-8")
-    # f.write(mydata)
-    # f.close
+    req = Request('https://www.worldometers.info/coronavirus/',headers ={'User-Agent':'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    mydata = webpage.decode("utf8")
+    f=open('webpage.html','w',encoding="utf-8")
+    f.write(mydata)
+    f.close
     file_obj = open('webpage.html','r',encoding="utf-8")
     data = file_obj.read()
     lexer = lex.lex()
